aee75267287283a90e239200a8507f17c0883c36
Anshiika Yadav <anshikaydv06@gmail.com>
2025-08-20 20:29:56 +0530

    changes in linking

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import ProductDetail from './pages/ProductDetail';
 import Cart from './pages/Cart';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-import About from './pages/About';// Make sure this file exists!
 import { CartProvider } from './context/CartContext';
 import { AuthProvider } from './context/AuthContext';
 import './App.css';
@@ -46,7 +45,6 @@ function App() {
               <Route path="/" element={<Home />} />
               <Route path="/category/:category" element={<Categories />} />
               <Route path="/product/:id" element={<ProductDetail />} />
-              <Route path="/about" element={<About />} />
               <Route path="/cart" element={<Cart />} />
               <Route path="/login" element={<Login />} />
               <Route path="/signup" element={<Signup />} />
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { Link } from "react-router-dom";
 import { ShoppingCart, User, Search, Menu, X, Sparkles } from 'lucide-react';
 import { useCart } from '../context/CartContext';
 import { useAuth } from '../context/AuthContext';
